0991ba4777b54f5c654468d1834da6bb3f8bffc2
aprabha7 <77811284+aprabha7@users.noreply.github.com>
2021-10-20 23:57:04 -0700

    User story 101 Task 117
    This page displays the rules and regulations set by the tournament manager

---
+++ frontend/src/components/FAQHeader.jsx
@@ -0,0 +1,23 @@
+
+import { Box, Link, List, ListItem, Typography } from '@material-ui/core'
+import React from 'react'
+import {FAQTopicsList } from "../demoJSONs/faq"
+const FAQHeader = ({ onTopicClick }) => {
+    return (
+       <Box mx={2} my={1}>
+            <Typography variant="h5">Frequently Asked Questions</Typography>
+            <Typography variant="body1">Select a topic category or scroll down to the page to find your question.</Typography>
+            <Box m={1}>
+            <List dense={true}>
+                {FAQTopicsList.map( (topic,i) => <ListItem>
+                     <Link component="button" href="#" onClick={()  => onTopicClick(topic,i)}  variant="body1" >{topic}
+                     </Link> 
+                     </ListItem>)}
+                </List>
+            </Box>
+        </Box>
+        
+    )
+}
+
+export default FAQHeader
