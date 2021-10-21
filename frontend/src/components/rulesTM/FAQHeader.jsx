fba368df5aa767c3af000dc85061f0dccf716274
aprabha7 <77811284+aprabha7@users.noreply.github.com>
2021-10-21 01:59:01 -0700

    Code refactoring done for user story 101, task 117.

--- frontend/src/components/rulesTM/FAQHeader.jsx
+++ frontend/src/components/rulesTM/FAQHeader.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 import {FAQTopicsList } from "../../demoJSONs/faq"
 const FAQHeader = ({ onTopicClick }) => {
     return (
-       <Box mx={2} my={1}>
+       <Box mx={2}>
             <Typography variant="h5">Frequently Asked Questions</Typography>
             <Typography variant="body1">Select a topic category or scroll down to the page to find your question.</Typography>
-            <Box m={1}>
+            <Box mt={1}>
             <List dense={true}>
                 {FAQTopicsList.map( (topic,i) => <ListItem>
                      <Link component="button" href="#" onClick={()  => onTopicClick(topic,i)}  variant="body1" >{topic}
--- frontend/src/components/rulesTM/RulesandRegulationsTM.jsx
+++ frontend/src/components/rulesTM/RulesandRegulationsTM.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Box, Grid, Link, Paper, Typography } from '@material-ui/core'
-import {FAQContent } from "../../demoJSONs/faq"
+import { FAQContent } from "../../demoJSONs/faq"
 import FAQHeader from './FAQHeader';
 
 const RulesandRegulationsTM = () => {
@@ -8,40 +8,40 @@ const RulesandRegulationsTM = () => {
     const [change, setChanged] = useState(false);
     useEffect(() => {
         window.scrollTo(0, 0)
-      }, [change])
-    const  handleOnClick = (index) => {
-            if(topicsRef.current[index]){
-                topicsRef.current[index].scrollIntoView({ 
-                behavior: "smooth", 
+    }, [change])
+    const handleOnClick = (index) => {
+        if (topicsRef.current[index]) {
+            topicsRef.current[index].scrollIntoView({
+                behavior: "smooth",
                 block: "start"
-                })
-            }
+            })
         }
+    }
 
     return (
         <Grid container direction="row">
             <Grid item >
                 <FAQHeader onTopicClick={(topic, index) => handleOnClick(index)} />
             </Grid>
-        {FAQContent.map((topic,i) =>  <Grid item >
-                <Box ref={el => topicsRef.current[i] = (el)} mx={1}  my={4}>
-                <Paper >   
-                <Box p={1}>
-                <Box p={1} m={1} style={{ backgroundColor: "#757ce8", color: "white"}} >
-                    <Typography variant="h5" >{topic.title}</Typography>
+            {FAQContent.map((topic, i) => <Grid item ref={el => topicsRef.current[i] = (el)}>
+                <Box  mx={1} mb={2} mt={9}>
+                    <Paper >
+                        <Box p={1}>
+                            <Box p={1} m={1} style={{ backgroundColor: "#757ce8", color: "white" }} >
+                                <Typography variant="h5" >{topic.title}</Typography>
+                            </Box>
+                            {topic.content.map((data, i) => <Box p={1} m={1} style={{ backgroundColor: i % 2 == 0 ? "#e5e5e5" : "" }}>
+                                <Typography variant="h6" >{data.question}</Typography>
+                                <Typography variant="body1">{data.answer}</Typography>
+                            </Box>)}
+                            <Box px={2}>
+                                <Link href="" onClick={() => setChanged(!change)} variant="body2">Return to the FAQ Menu</Link>
+                            </Box>
+                        </Box>
+
+                    </Paper>
                 </Box>
-                {topic.content.map( (data,i) => <Box p={1} m={1} style={{backgroundColor: i % 2 == 0 ? "#e5e5e5" : ""}}>
-                    <Typography variant="h6" >{data.question}</Typography>
-                    <Typography variant="body1">{data.answer}</Typography>
-                </Box>)}
-                <Box px={2}>
-                    <Link href="" onClick={() =>  setChanged(!change)} variant="body2">Return to the FAQ Menu</Link>
-                </Box>
-                </Box>
-            
-            </Paper>
-            </Box> 
-        </Grid>)}
+            </Grid>)}
         </Grid>
     )
 }
