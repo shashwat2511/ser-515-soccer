ec9d570296cc7825675536f0a9c663dbb859434f
aprabha7 <77811284+aprabha7@users.noreply.github.com>
2021-11-24 18:13:15 -0700

    User story 189 task 207, updating admin login page UI
    User story 189 task 207, updating admin login page UI with box components to enhance display.

--- frontend/src/components/AdminLogin.js
+++ frontend/src/components/AdminLogin.js
@@ -1,4 +1,4 @@
-import { Button, Checkbox, FormControlLabel, Grid, Paper, SvgIcon, TextField, Typography } from '@material-ui/core'
+import { Button, Checkbox, FormControlLabel, Grid, SvgIcon, TextField, Typography } from '@material-ui/core'
 import { Box } from '@mui/system'
 import React from 'react';
 
@@ -16,18 +16,37 @@ export const AdminLogin = () => {
       }
 
     return (
-        <Grid container component="main" sx={{height: '100vh'}}>
-            <Grid item xs={12} sm={18} md={15} component={Paper} elevation={6} square>
-                <Box sx={{my: 8, mx: 4, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <Grid container justifyContent="center">
+            <Grid item xs={5}>
+                <Box sx={{my:8, mx:4, display:'flex', flexDirection:'column', alignItems: 'center'}}>
                     <HomeIcon color="primary" />
                     <Typography variant="h5">Login</Typography>
-                    <Box component="form" onSubmit={handleSubmit} sx={{mt:2}}>
-                        <TextField sm={5} style={{width:'100%'}}  required id="username" label="User name" margin="normal" autoFocus />
-                        <TextField sm={5} style={{width:'100%'}}  required id="password" label="Password" margin="normal" autoFocus />
-                        <FormControlLabel control={<Checkbox value="remember" color="primary"/>} label="Remember me" />    
+                    &nbsp;
+                    <Box onSubmit={handleSubmit} sx={{mt:2}}>
+                    <Grid container direction="row" >
+                        <Grid item xs={12}>
+                            <Box p={1}>
+                                <TextField sm={5} style={{width:'100%'}} variant="outlined" required id="username" label="User name" margin="normal" autoFocus />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Box p={1}>
+                            <TextField sm={5} style={{width:'100%'}} variant="outlined" required id="password" label="Password" margin="normal" autoFocus />
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Box p={1}>
+                                <FormControlLabel control={<Checkbox value="remember" color="primary"/>} label="Remember me" />
+                            </Box>
+                        </Grid>    
+                    </Grid>
                     </Box>
                     &nbsp;
-                    <Button type="submit" fullwidth variant="contained" sx={{mt:3, mb:2}} color="primary">Login</Button>
+                    <Grid item xs={5}>
+                        <Box p={1}>
+                            <Button fullWidth variant="contained" color="primary">Login</Button>
+                        </Box>
+                    </Grid>
                 </Box>
             </Grid>
         </Grid>
