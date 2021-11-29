23a096dec5c5c681bd87c8dd495c9a5167f2b710
aprabha7 <77811284+aprabha7@users.noreply.github.com>
2021-11-28 20:08:13 -0700

    Updated faq page
    Resolved some bugs in faq page

--- frontend/src/demoJSONs/faq.js
+++ frontend/src/demoJSONs/faq.js
@@ -1,6 +1,6 @@
 
 export const FAQTopicsList = [
-    "Application", "Bracketing/Team Selection", "General", "Hotels/Travel", "Registration","Web Site"
+    "Application", "Bracketing/Team Selection", "General", "Hotels/Travel", "Registration"
 ]
 
 
@@ -10,19 +10,19 @@ export const FAQContent = [
         content: [
             {
                 question : "Do you have a paper application I can fill out?",
-                answer:"All applications are only accepted online at our web site. This will enable us to more accurately capture your team's data as well as reduce the time it takes to evaluate teams for acceptance after the application deadline. Our website has been a reliable tool for information, games schedules, and scores since our 2000 event and will continue to play a bigger role with each year."
+                answer:"All applications are only accepted online at our web site. This will enable us to more accurately capture your team's data as well as reduce the time it takes to evaluate teams for acceptance after the application deadline. Our website has been a reliable tool for information, games schedules, and scores and will continue to play a bigger role with each year."
             },
             {
                 question: "Do you want to check on the status of your team?",
-                answer: "Log in using your assigned team ID number (Like WSC123456789 or WFC 123456789) that was assigned to you when you registered. Go to the log in box under the Coaches tab in the left rail."
+                answer: "Log in using your assigned team ID number that was assigned to you when registered. Go to the log in box."
                 
             }, {
                 question: "How do I place my team in one division or the other?",
-                answer: "Your team will be placed in the division to be the most competitive with other teams that apply. However, you may help the selection committee by indicating your choice on the application form. Level 1-GOLD; Level 2-RED; Level 3-WHITE; & Level 4-BLUE divisions. If we only have 2 levels in an age group then only GOLD and RED will be used."
+                answer: "Your team will be placed in the division to be the most competitive with other teams that apply. However, you may help the selection committee by indicating your choice on the application form. Level 1-GREEN; Level 2-RED; Level 3-YELLOW; Level 4-BLUE & Level 5-PURPLE divisions. If we only have 2 levels in an age group then only BLUE and RED will be used."
             },
             {
                 question: "When do I send travel permit?",
-                answer: "DO NOT SEND A TRAVEL PERMIT WITH YOUR APPLICATION. This is only required when/if your team is accepted. Travel permits are REQUIRED for all teams who are registered outside Ohio South (OSYSA) except for US Club Soccer that does not require travel permits. You should submit your Pre Registration travel permit with your on line registration items."
+                answer: "DO NOT SEND A TRAVEL PERMIT WITH YOUR APPLICATION. This is only required when/if your team is accepted. Travel permits are REQUIRED for all teams who are registered outside Arizona except for US Club Soccer that does not require travel permits. You should submit your Pre Registration travel permit with your on line registration items."
             }
         ]
     },
@@ -31,12 +31,11 @@ export const FAQContent = [
         content: [
             {
                 question : "Does the Classic have 2 divisions per age group?",
-                answer:"Depending on the number of teams in any given age group applying to the adidas Warrior Halloween Fall Soccer Classic, some age groups may consist of a Gold, Red, White divisions depending upon numbers. The Gold Division will be the upper division, Red is middle and White is lower. However, not all age groups will have 2 or 3 levels of play."
+                answer:"Depending on the number of teams in any given age group applying to the APR Soccer management, some age groups may consist of a Green, Red, Yellow, Blue divisions depending upon numbers. However, not all age groups will have 2 or 3 levels of play."
             },
             {
                 question: "Where and when will the schedules be posted?",
-                answer: "The schedule may be posted as early as the the first week in October. If you are listed as the contact for the team, you will be sent an email when the schedules are posted. You can access the game schedules, scores and photos by clicking on the GAMES/SCORES tab in the red menu across the top of any page OR by selecting your division in the drop down menu along the left side of any page on the site. You may also search by Club."
-                
+                answer: "The schedule may be posted as early as the the first week in October. If you are listed as the contact for the team, you will be sent an email when the schedules are posted. You can access the game schedules, scores by selecting your division in the drop down menu along the left side of any page on the site. You may also search by Club."
             },
         ]
     },
@@ -49,12 +48,54 @@ export const FAQContent = [
             },
             {
                 question: "How much does the tournament cost?",
-                answer: `The fees are posted on the front page, in the Facts At A Glance, to the left of the photo. Also posted there are the dates, location, deadline, etc.
+                answer: `Contact the management for fee structure. Also posted there are the dates, location, deadline, etc.
                 This information also appears on the About page and on the Application Form Instructions (About, Register choices from the top menu)`
             },
             {
                 question: "Where can I find the Forms page?",
-                answer: "Go to www.warriorfallclassic.com and click on the word FORMS at the top of the page and enter your assigned wsc number in the box requested. The next screen is your teams Application Status page and then click on Get Duplication Forms. Then Select and print the form(s) you need."
+                answer: "Go to APR soccer management and click on the word FORMS at the top of the page. The next screen is your teams Application Status page and then click on Get Duplication Forms. Then Select and print the form(s) you need."
+            }
+        ]
+    },
+    {
+        title: "Hotels/Travel",
+        content: [
+            {
+                question: "I am a local team who does not require housing. Do I still need to book hotel rooms to be in the tournament?",
+                answer: "No. Only the teams who require housing during their participation in the tournament must book hotel. Site Search is our official hotel registrar can assist you as needed."
+            },
+            {
+                question: "Our team needs a hotel. Can you help us with that?",
+                answer: "You can call up the management team for assistance."
+            }
+        ]
+    },
+    {
+        title: "Registration",
+        content: [
+            {
+                question: "Can I register my team early if I have everything completed and so I do not have to attend registration?",
+                answer: "All registrations will be online for your convenience."
+            },
+            {
+                question: "Can we substitute the liability form with a generic one?",
+                answer: "NO!! You must use the liability release supplied with the acceptance packet."
+            },
+            {
+                question: "Does each team need a medical release form for all the players?",
+                answer: "While the tournament does not require that teams have a medical release form for each player, we highly recommend that they do have one and bring it with them."
+            },
+            {
+                question: "When are my Guest Players due?",
+                answer: "You may add guest players up until Friday night before the tournament."
+            },
+            {
+                question: "When is my roster frozen?",
+                answer: "Your roster is frozen on the Friday before the weekend tournament."
+            },
+            {
+                question: "Where can I find the forms I need?",
+                answer: "Click on forms and select the form you need."
             }
         ]
     }
