6d19082341c288ddd35d6750f9b5c94aa9d71635
shashwat2511 <spriyad4@asu.edu>
2021-11-28 23:24:17 -0700

    developed an about us page

---
+++ frontend/src/demoJSONs/aboutusdata.js
@@ -0,0 +1,61 @@
+export const AboutUsContent = [
+    {
+        headers:[
+            {
+                question : "WHEN",
+                answer:	"Memorial Day Weekend, May 27-29, 2022"
+            },
+            {
+                question : "WHERE",
+                answer:	"Dayton, Ohio"
+            },
+            {
+                question : "FEES",
+                answer : "u9/u10 $625; u10/u11/u12 $675; u12/u13/u14 $725; u15/u16/u17/u18/u19 $775"
+            },
+            {
+                question : "APPLICATION DEADLINE",
+                answer : "Sunday, Apr 17, 2022"
+            },
+            {
+                question : "PAY TO",
+                answer : "Warrior Soccer Club; adidas WARRIOR SOCCER CLASSIC, ATTN: SELECTION COMMITTEE, P.O. BOX 24232, DAYTON, OHIO 45424 Credit Cards Accepted for Application Fees"
+            }
+
+        ],
+        content: [
+            {
+                question : "Qualifications",
+                answer:"Applications are considered from youth teams which are duly registered and play in a scheduled league competition. Players must be currently registered with a team by their state or Provincial Association and will be required to produce their USYSA player passes. No player will be permitted to play on more than one team during the tournament. Under 15-19 teams maximum roster size is 22, but only 18 players may play during a single game."
+            },
+            {
+                question: "Referees",
+                answer: "National & state referees and assessors will be in attendance to facilitate referee advancement. If you know of anyone wishing to referee at the tournament, they should go to the Front Page of the website, under REFEREES tab and sign up now."
+                
+            }, {
+                question: "Team Selection",
+                answer: "Team acceptance to the adidas Warrior Soccer Classic will be completed by the Selection Committee within 10 days of the March 20th deadline. Notifications are sent to all teams. The selection process is assisted by the information provided to the tournament on your application - example - team records, level of play, out of town/state vs local teams (to achieve a balance), performance in post-season tournaments including the Warrior FALL Classic and Warrior SPRING Classic, etc. It is important to provide as much information as possible to assist the committee in properly evaluating your team. Cashing of the entry fee does not indicate acceptance to the tournament."
+            },
+            {
+                question: "WSC Pride",
+                answer: "This tournament has gained outstanding recognition for its quality teams, referees and sites of play and organization. This pride in our tournament comes from the enthusiasm and hard work of all the members of our Warrior Soccer Club who are dedicated to this event and to the over 25,000 players, coaches, and parents who attend each year."
+            },
+            {
+                question: "Tournament Director",
+                answer: "APR - 123-456-7890"
+            },
+            {
+                question: "Accommodations",
+                answer: "The Warrior Tournament is a stay to play tournament. A team MUST stay in a listed hotel to be accepted and participate in the tournament. The Warrior Housing Bureau (SiteSearch) has blocked rooms at the hotels listed at the best possible rates. Please click on the Find A Hotel link in the left hand column to secure your hotel rooms."
+            },
+            {
+                question: "Website",
+                answer: "Information about the adidas Warrior Soccer Classic (spring) can be found on the Internet at www.warriorclassic.com (Please NOTE that the website for the Fall Classic is www.warriorfallclassic.com). These sites feature a tournament overview, area maps and hotel accommodations, local restaurants and more. Check www.warriorclassic.com for updates before, during and after the event. Scores and photos will be added live from the site as information becomes available. Since photos will be taken of games, your players will likely have pictures taken of them and posted on the tournament web site. By making an application to the adidas Warrior Classic Tournament, you are giving your consent to have your team/child photographed."
+            },
+            {
+                question: "College Opportunities",
+                answer: "Teams U15 thru U19 may showcase your players to over 40 college coaches who attend the adidas Warrior Soccer Classic each year. Individual player profiles provided by your team will be distributed to each college coach as they seek to enhance their college programs. This is a not-to-be-missed opportunity. For more information contact Carol Maas at carolmsoccer@aol.com"
+            }
+        ]
+    }
+]
